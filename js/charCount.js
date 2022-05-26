e19b05136212dfa6dfad958f4ff32934ae9a3096
nhungbi <nhungvan16@gmail.com>
2022-05-25 23:17:49 -0500

    fn

--- js/charCount.js
+++ js/charCount.js
@@ -23,12 +23,12 @@ exports.charCount = function(str) {
         const nextElt = sortedArray[i+1]
 
         if (currentElt[1] === nextElt[1]) { //want to group all same value to sort keys based on abc
-            if (sameGroup.includes(currentElt)) {
+            if (sameGroup.includes(currentElt)) { //already include current so just add next only
                 sameGroup.push(nextElt)} 
             else {
                 sameGroup.push(currentElt,nextElt)}
             }
-        else { //no longer same value
+        else { //no longer same value/group, so can cocat to output and reset group 
             const sortGroup = sameGroup.sort((current,next) => current[0].localeCompare(next[0]))
             output = output.concat(sortGroup)
 
@@ -47,44 +47,3 @@ exports.charCount = function(str) {
     return output
 
 };
-// let result_2 = exports.charCount("an apple a day will keep the doctor away")
-// console.log(result_2)
-// console.log(exports.charCount("aaabbc"))
-// console.log(result_1[0][0] === "a")
-// console.log(result_1[0][1] === 3)
-// console.log(result_1[1][0] === "b")
-// console.log(result_1[1][1] === 2)
-// console.log(result_1[2][0] === "c")
-// console.log(result_1[2][1] === 1)
-
-
-// console.log(result_2[0][0] === "a")
-// console.log(result_2[0][1] === 6)
-// console.log(result_2[1][0] === "e")
-// console.log(result_2[1][1] === 4)
-// console.log(result_2[2][0] === "l")
-// console.log(result_2[2][1] === 3)
-// console.log(result_2[3][0] === "p")
-// console.log(result_2[3][1] === 3)
-// console.log(result_2[4][0] === "d")
-// console.log(result_2[4][1] === 2)
-// console.log(result_2[5][0] === "o")
-// console.log(result_2[5][1] === 2)
-// console.log(result_2[6][0] === "t")
-// console.log(result_2[6][1] === 2)
-// console.log(result_2[7][0] === "w", 'here1', result_2[7][0])
-// console.log(result_2[7][1] === 2)
-// console.log(result_2[8][0] === "y")
-// console.log(result_2[8][1] === 2)
-// console.log(result_2[9][0] === "c")
-// console.log(result_2[9][1] === 1)
-// console.log(result_2[10][0] === "h")
-// console.log(result_2[10][1] === 1)
-// console.log(result_2[11][0] === "i")
-// console.log(result_2[11][1] === 1)
-// console.log(result_2[12][0] === "k")
-// console.log(result_2[12][1] === 1)
-// console.log(result_2[13][0] === "n")
-// console.log(result_2[13][1] === 1)
-// console.log(result_2[14][0] === "r")
-// console.log(result_2[14][1] === 1)
